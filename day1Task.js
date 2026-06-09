bb04cb036a5e7494c1b06769c4d53877fac5a5b4
BagavathRam7 <bagavathram7@gmail.com>
2026-06-09 23:23:30 +0530

    Add files via upload

---
+++ day1Task.js
@@ -0,0 +1,7 @@
+let userName = "Bagavath Ram"
+let organization = "TCS"
+let contactNumber = 948888711
+let knowsAutomation = "Yes"
+let userPlaywright
+console.log(userName,organization,contactNumber,knowsAutomation,userPlaywright);
+console.log(typeof userName,typeof organization,typeof contactNumber,typeof knowsAutomation,typeof userPlaywright);
