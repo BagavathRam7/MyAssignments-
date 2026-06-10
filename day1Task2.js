3ca78c08a77322bde7a3cab2723b3a923721d525
BagavathRam7 <bagavathram7@gmail.com>
2026-06-10 13:33:44 +0530

    Add files via upload

---
+++ day1Task2.js
@@ -0,0 +1,20 @@
+const browserVersion= "Chrome";
+function getBrowserVersion(){
+    if (browserVersion = "Chrome") {
+
+        var browserVersion = "123.1.3.10";
+        console.log(browserVersion)
+        
+    }
+}
+ getBrowserVersion()
+
+
+function getBrowserVersion2(){
+    if (browserVersion == "Chrome"){
+
+        let browserVersion = "124.2.2.11";
+        console.log(browserVersion)
+    }
+}
+getBrowserVersion2()
